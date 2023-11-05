a206be4a91a39ec59d3e5e0ba375790477b2f253
manalsamine <manal.samine01@gmail.com>
2023-11-05 14:18:35 +0100

    project

---
+++ bornSimule/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Borne from './components/borne.vue'
+
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-white">
+    <div class=" w-full bg-slate-300 shadow-lg font-poppins p-2 font-bold text-lg text-center "> born simulation creted by manal samine & wafae zoubaire </div>
+  <div class="w-full bg-red-500 h-full">
+   <Borne/>
+  </div>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ bornSimule/src/components/borne.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="w-full h-full flex items-center justify-center">
+<div class="w-56 gap-2 flex flex-col justify-start items-center h-96 bg-white border-8 border-emerald-600 rounded-lg p-3 relative">
+    <div class="absolute left-full ml-2 top-0">
+ <div class="w-4 h-64 rounded-lg bg-black"></div>
+ <div class="">
+
+    <svg class=" left-20 top-72 w-14 h-14 rotate-180 " xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path d="M3 8h2v5c0 2.206 1.794 4 4 4h2v5h2v-5h2c2.206 0 4-1.794 4-4V8h2V6H3v2zm4-6h2v3H7zm8 0h2v3h-2z"></path></svg>
+ </div>
+
+
+    </div>
+<div class="w-full fill-emerald-500  flex flex-col justify-center items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" class=" w-8 h-8 fill-emerald-500" viewBox="0 0 24 24" ><path d="M17.168 8H13l.806-4.835A1 1 0 0 0 12.819 2H7.667a1 1 0 0 0-.986.835l-1.667 10A1 1 0 0 0 6 14h4v8l8.01-12.459A1 1 0 0 0 17.168 8z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" class=" w-8 h-8 fill-emerald-500" viewBox="0 0 24 24" ><path d="m20.772 10.155-1.368-4.104A2.995 2.995 0 0 0 16.559 4H7.441a2.995 2.995 0 0 0-2.845 2.051l-1.368 4.104A2 2 0 0 0 2 12v5c0 .738.404 1.376 1 1.723V21a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-2h12v2a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-2.277A1.99 1.99 0 0 0 22 17v-5a2 2 0 0 0-1.228-1.845zM7.441 6h9.117c.431 0 .813.274.949.684L18.613 10H5.387l1.105-3.316A1 1 0 0 1 7.441 6zM5.5 16a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 5.5 16zm13 0a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 18.5 16z"></path></svg>
+</div>
+<div class="w-2/3 h-24 bg-slate-300 shadow-lg rounded-lg"> 
+</div>
+<div class="w-full gap-1 flex justify-center items-center">
+<div class="w-4 h-4 bg-red-500 rounded-full"></div>
+<div class="w-4 h-4 bg-green-500 rounded-full"></div>
+<div class="w-4 h-4 bg-yellow-500 rounded-full"></div>
+<div class="w-4 h-4 bg-blue-500 rounded-full"></div>
+</div>
+<div class="w-1/3 aspect-square  rounded-lg bg-slate-400 flex items-center justify-center">
+    <svg class="w-8 h-8 fill-white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" ><path d="M12 6c3.537 0 6.837 1.353 9.293 3.809l1.414-1.414C19.874 5.561 16.071 4 12 4c-4.071.001-7.874 1.561-10.707 4.395l1.414 1.414C5.163 7.353 8.463 6 12 6zm5.671 8.307c-3.074-3.074-8.268-3.074-11.342 0l1.414 1.414c2.307-2.307 6.207-2.307 8.514 0l1.414-1.414z"></path><path d="M20.437 11.293c-4.572-4.574-12.301-4.574-16.873 0l1.414 1.414c3.807-3.807 10.238-3.807 14.045 0l1.414-1.414z"></path><circle cx="12" cy="18" r="2"></circle></svg>
+</div>
+</div>
+</div>
+</template>
+<script setup></script>
+<style></style>
